da07e61b98bd6c45c4c12761d3608279d55e73bc
peikleh <nevinleh@gmail.com>
2017-07-30 11:31:47 -0600

    Updated to lesson 5

---
+++ src/game/Background.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2017 nevin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package game;
+
+/**
+ *
+ * @author nevin
+ */
+public class Background {
+
+    private int bgX,
+            bgY,
+            speedX;
+
+    public Background(int x, int y) {
+        bgX = x;
+        bgY = y;
+        speedX = 0;
+    }
+
+    public void update() {
+        bgX += speedX;
+        if (bgX <= -2160) {
+            bgX += 4320;
+        }
+    }
+
+    public int getBgX() {
+        return bgX;
+    }
+
+    public void setBgX(int bgX) {
+        this.bgX = bgX;
+    }
+
+    public int getBgY() {
+        return bgY;
+    }
+
+    public void setBgY(int bgY) {
+        this.bgY = bgY;
+    }
+
+    public int getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+
+}
---
+++ src/game/Robot.java
@@ -0,0 +1,197 @@
+package game;
+
+/*
+ * Copyright (C) 2017 nevin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ *
+ * @author nevin
+ */
+import java.awt.Graphics;
+
+public class Robot {
+
+    // Constants are Here
+    final int JUMPSPEED = -15;
+    final int MOVESPEED = 5;
+    final int GROUND = 382;
+
+    private int centerX = 100;
+    private int centerY = GROUND;
+    private boolean jumped = false;
+    private boolean movingLeft = false;
+    private boolean movingRight = false;
+    private boolean ducked = false;
+
+    private static Background bg1 = StartingClass.getBg1();
+    private static Background bg2 = StartingClass.getBg2();
+
+    private int speedX = 0;
+    private int speedY = 1;
+
+    public void update() {
+
+        // Moves Character or Scrolls Background accordingly.
+        if (speedX < 0) {
+            centerX += speedX;
+        }
+        if (speedX == 0 || speedX < 0) {
+            bg1.setSpeedX(0);
+            bg2.setSpeedX(0);
+
+        }
+        if (centerX <= 200 && speedX > 0) {
+            centerX += speedX;
+        }
+        if (speedX > 0 && centerX > 200) {
+            bg1.setSpeedX(-MOVESPEED);
+            bg2.setSpeedX(-MOVESPEED);
+        }
+
+        // Updates Y Position
+        centerY += speedY;
+        if (centerY + speedY >= GROUND) {
+            centerY = GROUND;
+        }
+
+        // Handles Jumping
+        if (jumped == true) {
+            speedY += 1;
+
+            if (centerY + speedY >= GROUND) {
+                centerY = GROUND;
+                speedY = 0;
+                jumped = false;
+            }
+
+        }
+
+        // Prevents going beyond X coordinate of 0
+        if (centerX + speedX <= 60) {
+            centerX = 61;
+        }
+    }
+
+    public void moveRight() {
+        if (ducked == false) {
+            speedX = MOVESPEED;
+        }
+    }
+
+    public void moveLeft() {
+        if (ducked == false) {
+            speedX = -MOVESPEED;
+        }
+    }
+
+    public void stopRight() {
+        setMovingRight(false);
+        stop();
+    }
+
+    public void stopLeft() {
+        setMovingLeft(false);
+        stop();
+    }
+
+    private void stop() {
+        if (isMovingRight() == false && isMovingLeft() == false) {
+            speedX = 0;
+        }
+
+        if (isMovingRight() == false && isMovingLeft() == true) {
+            moveLeft();
+        }
+
+        if (isMovingRight() == true && isMovingLeft() == false) {
+            moveRight();
+        }
+
+    }
+
+    public void jump() {
+        if (jumped == false) {
+            speedY = JUMPSPEED;
+            jumped = true;
+        }
+
+    }
+
+    public int getCenterX() {
+        return centerX;
+    }
+
+    public int getCenterY() {
+        return centerY;
+    }
+
+    public boolean isJumped() {
+        return jumped;
+    }
+
+    public int getSpeedX() {
+        return speedX;
+    }
+
+    public int getSpeedY() {
+        return speedY;
+    }
+
+    public void setCenterX(int centerX) {
+        this.centerX = centerX;
+    }
+
+    public void setCenterY(int centerY) {
+        this.centerY = centerY;
+    }
+
+    public void setJumped(boolean jumped) {
+        this.jumped = jumped;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+
+    public void setSpeedY(int speedY) {
+        this.speedY = speedY;
+    }
+
+    public boolean isDucked() {
+        return ducked;
+    }
+
+    public void setDucked(boolean ducked) {
+        this.ducked = ducked;
+    }
+
+    public boolean isMovingRight() {
+        return movingRight;
+    }
+
+    public void setMovingRight(boolean movingRight) {
+        this.movingRight = movingRight;
+    }
+
+    public boolean isMovingLeft() {
+        return movingLeft;
+    }
+
+    public void setMovingLeft(boolean movingLeft) {
+        this.movingLeft = movingLeft;
+    }
+
+}

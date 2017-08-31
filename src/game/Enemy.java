c22a17b42690fb4664930c60c9ffd43ad0acac28
Nevin <nevinleh@gmail.com>
2017-08-31 13:33:47 -0600

    Finished unit 3

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -27,10 +27,14 @@ public class Enemy {
     private int maxHealth, currentHealth, power, speedX, centerX, centerY;
     private Background bg = StartingClass.getBg1();
     public Rectangle r = new Rectangle(0, 0, 0, 0);
+    public int health = 5;
+    private int movementSpeed;
+    private Robot robot = StartingClass.getRobot();
 
     public void update() {
+        follow();
         centerX += speedX;
-        speedX = bg.getSpeedX() * 5;
+        speedX = bg.getSpeedX() * 5 + movementSpeed;
         r.setBounds(centerX - 25, centerY - 25, 50, 60);
 
         if (r.intersects(Robot.yellowRed)) {
@@ -43,6 +47,23 @@ public class Enemy {
             System.out.println("collision");
 
         }
+    }
+
+    public void follow() {
+
+        if (centerX < -95 || centerX > 810) {
+            movementSpeed = 0;
+        } else if (Math.abs(robot.getCenterX() - centerX) < 5) {
+            movementSpeed = 0;
+        } else {
+
+            if (robot.getCenterX() >= centerX) {
+                movementSpeed = 1;
+            } else {
+                movementSpeed = -1;
+            }
+        }
+
     }
 
     public void die() {
